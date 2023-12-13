8fc47c22918ef02752b711543e1a9511aa90f699
stak <Spartak.Mankovskyy@infineon.com>
2023-12-13 14:49:19 +0200

    L5_hash Hashing demo programs

---
+++ python/L5_hash/sha_text_hash_demo.py
@@ -0,0 +1,27 @@
+from cryptography.hazmat.primitives import hashes
+import common.utils as utils
+
+# Define test to be hashed and make its encoding to UTF-8
+message = "Let the cat out of the bag"
+message_as_bytes = message.encode("utf-8")
+
+# Calculate SHA-256
+digest = hashes.Hash(hashes.SHA256())
+digest.update(message_as_bytes)
+my_hash = digest.finalize()
+
+# Write results to binary file
+utils.WriteBinaryFile("message.dat", message_as_bytes )
+utils.WriteBinaryFile("message_hash.dat", my_hash )
+
+print(f"my_hash: {my_hash.hex()}")
+print("byte my_hash[] = " + utils.MakeCArray_Invert(my_hash.hex()) + ";")
+
+# Lets try to change only one letter in the message and recalculate hash for it
+message2 = "Let the fat out of the bag"
+message2_as_bytes = message2.encode("utf-8")
+digest = hashes.Hash(hashes.SHA256())
+digest.update(message2_as_bytes)
+my_hash2 = digest.finalize()
+
+print(f"my_hash2: {my_hash2.hex()}")
---
+++ python/L5_hash/sha_tv_hash_demo.py
@@ -0,0 +1,30 @@
+from cryptography.hazmat.primitives import hashes
+import common.utils as utils
+
+#  CAVS 11.0
+#  "SHA-1 ShortMsg" information
+#  SHA-1 tests are configured for BYTE oriented implementations
+#  Generated on Tue Mar 15 08:23:35 2011
+#  File: SHA1ShortMsg.rsp
+# SHA-1 NIST Test Vector
+Len = 32
+Msg = '549e959e'
+MD = 'b78bae6d14338ffccfd5d5b5674a275f6ef9c717'
+
+# Define test to be hashed and make its encoding to UTF-8
+message = bytes.fromhex(Msg)
+
+# Calculate SHA-1 using single update
+digest = hashes.Hash(hashes.SHA1())
+digest.update(message)
+my_hash = digest.finalize()
+print(f"my_hash: {my_hash.hex()}")
+
+# Calculate SHA-1 using 2 updates
+message1 = message[0:2] # The first part of the message
+message2 = message[2:]  # The second part of the message
+digest2 = hashes.Hash(hashes.SHA1())
+digest2.update(message1) # process part 1 (update #1)
+digest2.update(message2) # process part 2 (update #2)
+my_hash2 = digest2.finalize()
+print(f"my_hash2: {my_hash2.hex()}")
