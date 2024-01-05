3bea2df09557f7176eb5f4eb812d2031a8f32d4f
stak <Spartak.Mankovskyy@infineon.com>
2024-01-05 17:13:47 +0200

    L10_wolf_intro Qt project

---
+++ c/L10_wolf_intro/wolf_intro/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "shared_functions.h"
+#include <wolfssl/wolfcrypt/types.h>
+#include <wolfssl/wolfcrypt/wc_port.h>
+#include <wolfssl/wolfcrypt/random.h>
+
+#define RAND_NUMBER_BYTE_SIZE (8)
+
+WC_RNG my_rand; /* random number generator structure */
+int    ret;     /* returned value */
+/* Buffer to store random numbers */
+byte   buffer[RAND_NUMBER_BYTE_SIZE];
+
+int main()
+{
+    printf("Hello World!\n");
+
+    if ((ret = wolfCrypt_Init()) != 0) {
+        printf("wolfCrypt_Init failed %d\n", (int)ret);
+    }
+
+    ret = wc_InitRng(
+        &my_rand
+        );
+
+    if (0 != ret) {
+        printf("Error during random number generator initialization\n");
+    }
+    else {
+        printf("Random number generator initialized\n");
+    }
+
+    ret = wc_RNG_GenerateBlock(
+        &my_rand,
+        buffer,
+        RAND_NUMBER_BYTE_SIZE
+        );
+
+    if (0 != ret) {
+        printf("Error during random number generation\n");
+    }
+    else {
+        printf("Generated: ");
+        PrintHex(buffer, RAND_NUMBER_BYTE_SIZE);
+    }
+
+    ret = wc_FreeRng(&my_rand);
+    if (0 != ret) {
+        printf("Error during random number generator deinitialization");
+    }
+
+    if ((ret = wolfCrypt_Cleanup()) != 0) {
+        printf("wolfCrypt_Cleanup failed %d\n", (int)ret);
+    }
+
+    printf("completed\n");
+
+    return 0;
+}
