4c6d306317cbd80b5dd6ccad9b109c65f25fb805
Spartak <mspartak7@gmail.com>
2025-03-24 14:51:08 +0200

    RSA key size set to 1024

--- python/L7_asym_p2/rsa_keygen.py
+++ python/L7_asym_p2/rsa_keygen.py
@@ -4,7 +4,7 @@ import common.utils as utils
 
 # Call function from utilities module to generate RSA keys pair with given bit size
 # and store to DER and PEM files.
-rsa_key_bitsize = 512
+rsa_key_bitsize = 1024
 utils.RsaGenerateKeys(rsa_key_bitsize, "demo")
 
 print("completed")
