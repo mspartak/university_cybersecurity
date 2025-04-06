6d96cd71f6a5a2053350748f4856ea0f0b3be0e1
Spartak <mspartak7@gmail.com>
2025-04-06 20:07:50 +0300

    RSA Key size set to 1024. Put signature check under try except.

--- python/L8_sign/generate_keys.py
+++ python/L8_sign/generate_keys.py
@@ -1,6 +1,6 @@
 import common.utils as utils
 
 # Generate RSA keys pair
-utils.RsaGenerateKeys(512, "rsakey")
+utils.RsaGenerateKeys(1024, "rsakey")
 
 print("completed")
--- python/L8_sign/rsa_verify.py
+++ python/L8_sign/rsa_verify.py
@@ -13,17 +13,19 @@ signature = utils.ReadBinaryFile("signature.sign")
 # Load message from binary file
 message_bytes = utils.ReadBinaryFile("signed_message.txt")
 
-# Verify Signature using RSA and PSS padding
-rsa_public_key_obj.verify(
-    signature,
-    message_bytes,
-    padding.PSS(
-        mgf=padding.MGF1(hashes.SHA256()),
-        salt_length=padding.PSS.MAX_LENGTH
-    ),
-    hashes.SHA256()
-)
-
-print("Signature is valid. Otherwise you will get an exception.")
+try:
+    # Verify Signature using RSA and PSS padding
+    rsa_public_key_obj.verify(
+        signature,
+        message_bytes,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    print("Signature is valid.")
+except Exception as e:
+    print(f"ERROR: Signature is invalid: {e}")
 
 print("completed")
